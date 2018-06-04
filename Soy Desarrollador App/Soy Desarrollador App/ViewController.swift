5a2ae5b9e0b347f373df0511f76b8ed21e080a20
GabrielTobarP <39927912+GabrielTobarP@users.noreply.github.com>
2018-06-04 17:18:22 -0500

    Add files via upload

---
+++ Soy Desarrollador App/Soy Desarrollador App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soy Desarrollador App
+//
+//  Created by Gabriel Tobar on 6/4/18.
+//  Copyright © 2018 Tu Nombre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
